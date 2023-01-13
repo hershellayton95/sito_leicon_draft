0b5d76610b9699e491bc04b789dd4f2b1824dabc
hershellayton95 <filidm5795@gmail.com>
2023-01-13 20:02:54 +0100

    add argonavis image and added size property

--- src/components/layout/CollaborationSection.js
+++ src/components/layout/CollaborationSection.js
@@ -4,8 +4,8 @@ const PatnerSection = {
             <h1 class="display-5 wow fadeInUp">Le nostre collaborazioni</h1>
             <p class="fs-4 mb-5 wow fadeInUp">Siamo lieti di presentarvi le persone e le aziende ed associazioni con cui abbiamo l'onore di lavorare:</p>
             <div class="container-sm row justify-content-center" style="max-width:750px">
-                <div v-for="item in input" class="col-6 col-md-4 text-center mt-3">
-                    <img v-if="item.image" :src="item.image" alt="MAPOD4D" style="width:auto; height: 100px; cursor:pointer">
+                <div v-for="item in input" class="col-6 col-md-4 text-center mt-3 overflow-hidden d-flex flex-column justify-content-end align-items-center">
+                    <img v-if="item.image" :src="item.image" alt="MAPOD4D" :width="item.size" style="height: auto; cursor:pointer" >
                             <h5 class="card-title mt-3">
                             {{item.title}}
                             </h5>
@@ -18,23 +18,28 @@ const PatnerSection = {
             input: [
                 {
                     title: "MAPOD4D",
-                    image: "./img/mapod.png"
+                    image: "./img/mapod.png",
+                    size: 100
                 },
                 {
                     title: "ROBDEV",
-                    image: ""
+                    image: "",
+                    size: 90
                 },
                 {
                     title: "ARCHEOS TEC",
-                    image: "./img/archeostec.jpg"
+                    image: "./img/archeostec.jpg",
+                    size: 150
                 },
                 {
                     title: "ARGONAVIS srl",
-                    image: "./img/argonavis.jpg"
+                    image: "./img/argonavis.jpg",
+                    size: 200
                 },
                 {
                     title: "OSTEOARC",
-                    image: "./img/osteoarc.jpg"
+                    image: "./img/osteoarc.jpg",
+                    size: 200
                 }
             ]
         }
