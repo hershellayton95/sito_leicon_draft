258587f9113873cb4ddff1345fe5ae9f8beeb904
hershellayton95 <filidm5795@gmail.com>
2023-01-22 21:35:00 +0100

    update and some fixes

--- src/components/layout/CollaborationSection.js
+++ src/components/layout/CollaborationSection.js
@@ -1,10 +1,10 @@
 const PatnerSection = {
-    template:`
+    template: `
         <div class="container text-center py-5 d-flex flex-column align-items-center">
             <h1 class="display-5 wow fadeInUp">Le nostre collaborazioni</h1>
             <p class="fs-4 mb-5 wow fadeInUp">Siamo lieti di presentarvi le persone e le aziende ed associazioni con cui abbiamo l'onore di lavorare:</p>
             <div class="container-sm row justify-content-center" style="max-width:750px">
-                <div v-for="item in input" class="col-6 col-md-4 text-center mt-3 overflow-hidden d-flex flex-column justify-content-end align-items-center">
+                <div v-for="item in input" class="col-12 col-md-4 text-center mt-3 overflow-hidden d-flex flex-column justify-content-end align-items-center">
                     <a :href="item.href" target="_blank">
                         <img v-if="item.image" :src="item.image" alt="MAPOD4D" :width="item.size" style="height: auto; cursor:pointer" >
                                 <h5 class="card-title mt-3">
@@ -15,14 +15,14 @@ const PatnerSection = {
             </div>
         </div>
     `,
-    data(){
+    data() {
         return {
             input: [
                 {
                     title: "MAPOD4D",
                     href: "https://www.mapod4d.it/",
                     image: "./img/mapod.png",
-                    size: 100
+                    size: 120
                 },
                 {
                     title: "ROBDEV",
